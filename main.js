c62c1621743868162b388858b1e957f4b7fe5042
everthis <wwwaap@gmail.com>
2017-05-03 23:24:03 +0800

    add resize feature.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 let path = require('path');
 let Koa = require('koa');
 let fs = require('fs');
+let gm = require('gm');
 let koaStatic = require('koa-static');
 let Router = require('koa-router');
 let koaBody = require('koa-body');
@@ -24,7 +25,9 @@ let koaBodyMW = koaBody({
 		uploadDir,
 		keepExtensions: true,
 		onFileBegin: function(name, file) {
-			file.publicFileName = path.basename(file.path);
+			file.dir = path.dirname(file.path);
+			file.publicOriginal = path.basename(file.path);
+			file.publicAvatar = file.publicOriginal.replace(/(\.[\w\d_-]+)$/i, '_thumbnail$1');
 		}
 	}
 });
@@ -33,7 +36,8 @@ function res() {
 	return function(fbd, status, msg) {
 		defaultRes.status = defaultRes.status === 'error' ? 'error' : status;
 		if(msg) defaultRes.message.push(msg);
-		if(fbd.publicFileName) defaultRes.data.url = fbd.publicFileName;
+		if(fbd.publicOriginal) defaultRes.data.original = fbd.publicOriginal;
+		if(fbd.publicAvatar) defaultRes.data.thumbnail = fbd.publicAvatar;
 		return defaultRes;
 	}
 }
@@ -47,12 +51,17 @@ function process(fbd) {
 	if ( ['image/jpeg', 'image/png'].indexOf(fbd.type) === -1 ) {
 		return resCb(fbd, 'error', 'only jpeg/png allowed.')
 	}
+	gm(fbd.path)
+	.resize(240, 240)
+	.noProfile()
+	.write(path.join(fbd.dir, fbd.publicAvatar), function (err) {
+	  if (err) console.log(err);
+	});
 	return resCb(fbd, 'sucess');
 }
 
 router.post('/upload', koaBodyMW,
   (ctx, next) => {
-    console.log(ctx.request.body);
     let bd = ctx.request.body;
     let fbd = bd.files.file;
     ctx.body = process(fbd);
